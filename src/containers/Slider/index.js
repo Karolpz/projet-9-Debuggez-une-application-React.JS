7dbb915ac61abb3aef99edb502e4b72285392686
Karolpz <carolinelpz34@gmail.com>
2025-01-22 14:28:26 +0100

    mise en place des filtres

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,43 +7,42 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
+
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
 
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
-      5000
-    );
-    return () => clearTimeout(nextCard);
-  };
   useEffect(() => {
-    nextCard();
-  }, [index, byDateDesc]);
+    const nextCard = setTimeout(() => {
+      setIndex(index < byDateDesc.length - 1 ? index + 1 : 0);
+    }, 5000);
+    return () => clearTimeout(nextCard);
+  });
+
 
   return (
     <div className="SlideCardList">
+
       {byDateDesc?.map((event, idx) => (
         <div key={event.id}>
           <div
             className={`SlideCard SlideCard--${index === idx ? "display" : "hide"
               }`}
           >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
+          </div>
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+              {byDateDesc.map((bullet, radioIdx) => (
                 <input
-                  key={`radio-${event.id}`}
+                  key={`radio-${bullet.id}`}
                   type="radio"
                   name="radio-button"
                   checked={index === radioIdx}
